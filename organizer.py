76efeb25043ae354c45591513b29e2ab2741df8c
Soubhik Sen <sensoubhik2001@gmail.com>
2025-10-18 10:00:28 +0000

    Add CLI arg

--- organizer.py
+++ organizer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Optional
 from utils import load_categories, categorize_file, move_file
 import sys
 
@@ -11,11 +11,16 @@ else:
     BASE_DIR:Path = Path(__file__).resolve().parent 
 
 CATEGORIES_FILE:Path = BASE_DIR / "categories.json"
-DOWNLOADS_FOLDER:Path = Path.home() / "Documents" / "Testing"
-
 FILE_CATEGORIES: Dict[str, List[str]] = load_categories(CATEGORIES_FILE)
 
-def organize_downloads() -> None:
+def organize_downloads(target_dir: Optional[Path] = None) -> None:
+    if target_dir is None:
+        return
+    if not target_dir.exists() or not target_dir.is_dir():
+        print(f"❌ Error: {target_dir} is not a valid directory.")
+        return
+    
+    DOWNLOADS_FOLDER:Path = target_dir
     print(f"Organizing files in: {DOWNLOADS_FOLDER}\n")
     count = 0
     for item in DOWNLOADS_FOLDER.iterdir():
@@ -30,4 +35,9 @@ def organize_downloads() -> None:
     print(f"Organization complete! {count} files moved.")
 
 if __name__ == "__main__":
-    organize_downloads()
+    if len(sys.argv) > 1:
+        input_path = Path(sys.argv[1]).expanduser().resolve()
+        organize_downloads(input_path)
+    else:
+        print("Give Location")
+        exit(1)
